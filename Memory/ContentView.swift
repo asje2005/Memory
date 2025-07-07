15755684bec982375e970245d1b2dc49cdcab26f
Aaron <asje2005@yahoo.com>
2025-07-07 10:42:47 -0400

    SwiftUI learning Stanford CS193P'23

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -16,28 +16,36 @@ struct ContentView: View {
             CardView()
         }
         .foregroundColor(.orange)
-        .padding()
+        .padding(.horizontal)
     }
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
+            var base : RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp{
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 3)
                 Text("🫎")
                     .font(.largeTitle)
             } else{
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
+        }
+        .onTapGesture{
+            isFaceUp.toggle()
         }
     }
 }
-#Preview {
-    ContentView()
+
+struct ContenetView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
 }
