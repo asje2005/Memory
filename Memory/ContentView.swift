a3d051c529c809f27c8df8a716efe7ba6042711a
Aaron <asje2005@yahoo.com>
2025-07-07 20:01:34 -0400

    adding buttons for images and counting cards up and down

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -8,46 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["🦋","🐞","🐜","🐝","🪳"]
-    //:Array<String> =
+    let emojis = ["🦋","🐞","🐜","🐝","🪳","🦟","🪱","🪲"]
+    
+    @State var cardCount: Int = 4
     
     var body: some View {
-        HStack{
-            ForEach(emojis.indices, id: \.self){ index in
-                CardView(content: emojis[index])
+        VStack {
+            HStack{
+                ForEach(0..<cardCount, id: \.self){ index in
+                    CardView(content: emojis[index])
+                }
             }
-        }
-        .foregroundColor(.orange)
-        .padding(.horizontal)
+            .foregroundColor(.orange)
+            
+            HStack{
+                Button(action: {
+                    if cardCount > 1 {
+                        cardCount -= 1
+                    }
+                }, label: {
+                    Image(systemName: "rectangle.stack.fill.badge.minus")
+                })
+                
+                Spacer()
+                Button(action: {
+                    if cardCount < emojis.count {
+                        cardCount += 1
+                    }
+                }, label: {
+                    Image(systemName: "rectangle.stack.fill.badge.plus")
+                })
+            }
+                .imageScale(.large)
+                .font(.largeTitle)
+            }
+        .padding()
     }
 }
-
-struct CardView: View {
-    let content: String
-    @State var isFaceUp = true
+    struct CardView: View {
+        let content: String
+        @State var isFaceUp = true
+        
+        var body: some View {
+            ZStack {
+                var base : RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+                if isFaceUp{
+                    base.fill(.white)
+                    base.strokeBorder(lineWidth: 3)
+                    Text(content).font(.largeTitle)
+                } else{
+                    base.fill()
+                }
+            }
+            .onTapGesture{
+                isFaceUp.toggle()
+            }
+        }
+    }
     
-    var body: some View {
-        ZStack {
-            var base : RoundedRectangle = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp{
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 3)
-                Text(content).font(.largeTitle)
-            } else{
-                base.fill()
-            }
-        }
-        .onTapGesture{
-            isFaceUp.toggle()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .preferredColorScheme(.light)
+            ContentView()
+                .preferredColorScheme(.dark)
         }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.light)
-        ContentView()
-            .preferredColorScheme(.dark)
-    }
-}
